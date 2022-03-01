020fe8b2a687401c366323f1e03c9160cacb40ce
Tantoony <tantoony.id@gmail.com>
2022-03-01 18:03:50 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { NestFastifyApplication, FastifyAdapter } from '@nestjs/platform-fastify
 async function bootstrap() {
   config({ path: __dirname.toString().replace(__dirname.toString().split('/').pop(), './.env' )});
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
-  await app.listen(5000);
+  await app.listen(5000, "127.0.0.1");
 }
 
 bootstrap();
