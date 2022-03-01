d0fc361154acf6fee213716545a09b994726d3d1
Tantoony <tantoony.id@gmail.com>
2022-03-01 21:21:52 +0300

    .

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,12 @@ import { NestFastifyApplication, FastifyAdapter } from '@nestjs/platform-fastify
 async function bootstrap() {
   config({ path: __dirname.toString().replace(__dirname.toString().split('/').pop(), './.env' )});
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
+  app.enableCors({
+    "origin": "*",
+    "methods": "GET",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
+  });
   await app.listen(5000, "127.0.0.1");
 }
 
