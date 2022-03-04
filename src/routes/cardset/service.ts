4cb6c531ae22c7dd97fcca0107b92b3b52adaf5e
Tantoony <tantoony.id@gmail.com>
2022-03-04 09:06:03 +0300

    Update service.ts

--- src/routes/cardset/service.ts
+++ src/routes/cardset/service.ts
@@ -8,13 +8,25 @@ export class DispService {
     constructor(@InjectModel("cardsets") private cardModel: Model<CardDoc>) {
     }
 
-    async show(): Promise<CardDoc[]> {
+    async show(): Promise<{
+        name: string,
+        tags: string[],
+        stars: number,
+        suffix: string | null
+    }[]> {
         const myCard = await this.cardModel.find();
-        return myCard;
+        return myCard.map((card) => {
+            return {
+                name: card.name,
+                tags: card.tags,
+                stars: card.stars,
+                suffix: card.suffix || null
+            }
+        })
     }
 
     async softCreate({ name, tags, stars, suffix }): Promise<Record<string, string>> {
-        if (!suffix) suffix = name;
+        if (!suffix) suffix = null;
         await this.cardModel.create({
             name,
             tags: tags.split('.'),
