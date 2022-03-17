e11befe0448721ed29b94f316b025162a2c65c0b
Tantoony <tantoony.id@gmail.com>
2022-03-17 15:23:05 +0300

    .

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,23 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from "@nestjs/mongoose";
-import { DispModule } from "./routes/cardset/module";
-import { DcModule } from "./routes/dc/module";
-import { PortModule } from "./routes/port/module";
+import { MongooseModule } from '@nestjs/mongoose';
+import { DispModule } from './routes/cardset/module';
+import { DcModule } from './routes/dc/module';
+import { PortModule } from './routes/port/module';
 @Module({
-    imports: [
-        MongooseModule.forRoot('mongodb://127.0.0.1:27017/nest', {
-            user: "Tantoony",
-            pass: "dU++%_6u69",
-            authSource: "admin"
-        }),
-        DispModule,
-        DcModule,
-        PortModule
-    ],
-    controllers: [AppController],
-    providers: [AppService],
+  imports: [
+    MongooseModule.forRoot('mongodb://127.0.0.1:27017/nest', {
+      user: 'Tantoony',
+      pass: 'c**sZ7DMhM&Dh5jU',
+      authSource: 'admin',
+    }),
+    DispModule,
+    DcModule,
+    PortModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule {
-}
+export class AppModule {}
